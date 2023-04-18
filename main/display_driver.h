84d0a2773ca0f428bc93d483f4d29b650adf20dc
Christian Speckner <christian.speckner@mayflower.de>
2023-04-18 23:18:18 +0200

    Partial mode.

--- main/display_driver.h
+++ main/display_driver.h
@@ -5,15 +5,21 @@
 
 namespace display_driver {
 
+enum class mode { partial, full, undefined };
+
 bool init();
 
 void refresh_display();
 
 void set_mode_full();
+void set_mode_partial();
+mode get_mode();
 
 void turn_off();
+void turn_on();
 
 void display_full(const uint8_t* image);
+void display_partial(const uint8_t* image_old, const uint8_t* image_new);
 
 }  // namespace display_driver
 
--- main/main.cxx
+++ main/main.cxx
@@ -5,21 +5,53 @@
 
 #include "display_driver.h"
 #include "esp_log.h"
+#include "esp_random.h"
 
 extern "C" void app_main(void) {
     display_driver::init();
 
-    display_driver::set_mode_full();
+    uint8_t* image_old = static_cast<uint8_t*>(malloc(300 * 50));
+    uint8_t* image_new = static_cast<uint8_t*>(malloc(300 * 50));
+    uint8_t ctr = 0;
 
-    uint8_t* image = static_cast<uint8_t*>(malloc(300 * 50));
+    while (true) {
+        constexpr size_t N = 30;
+        uint8_t rx[N];
+        uint8_t ry[N];
 
-    for (size_t y = 0; y < 300; y++)
-        for (size_t x = 0; x < 50; x++) image[x + y * 50] = (y % 4 == 0) ? 0x00 : 0xee;
+        for (int i = 0; i < N; i++) {
+            rx[i] = esp_random() % 47;
+            ry[i] = esp_random() % 276;
+        }
 
-    display_driver::display_full(image);
+        for (int y = 0; y < 300; y++)
+            for (int x = 0; x < 50; x++) {
+                image_new[y * 50 + x] = 0xff;
 
-    display_driver::refresh_display();
-    display_driver::turn_off();
+                for (int i = 0; i < N; i++) {
+                    if ((x >= rx[i] && x - rx[i] < 3 && y >= ry[i] && y - ry[i] < 24)) image_new[y * 50 + x] = 0x00;
+                }
+            }
 
-    free(image);
+        if ((ctr++ & 0x07) == 0x00) {
+            display_driver::set_mode_full();
+        } else {
+            display_driver::set_mode_partial();
+        }
+
+        if (display_driver::get_mode() == display_driver::mode::full)
+            display_driver::display_full(image_new);
+        else
+            display_driver::display_partial(image_old, image_new);
+
+        display_driver::refresh_display();
+
+        display_driver::turn_off();
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        display_driver::turn_on();
+
+        uint8_t* tmp = image_new;
+        image_new = image_old;
+        image_old = tmp;
+    }
 }
